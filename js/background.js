dda95f7ed9e1c1bbe1ce00a4c5c49b2fa3a942b8
HSH-5566 <cat5566@kyonggi.ac.kr>
2021-12-01 18:44:11 +0900

    [Refactor]Css-flex

--- js/background.js
+++ js/background.js
@@ -1,6 +1,8 @@
 const img = ["0.jpg","1.jpg","2.jpg","3.jpg"];
 
 const randomImg = img[Math.floor(Math.random()*img.length)];
-const selectImg = document.createElement("img");
-selectImg.src = `img/${randomImg}`;
-document.body.appendChild(selectImg); 
+// const selectImg = document.createElement("img");
+// selectImg.src = `img/${randomImg}`;
+// document.body.appendChild(selectImg); 
+const selectImg = document.querySelector("body");
+selectImg.style.backgroundImage = `url(img/${randomImg})`;
